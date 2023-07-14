d6a18c3f77e3e50f10a08100283fc7d42f7a6c1d
Otmane75 <92949728+Otmane75@users.noreply.github.com>
2023-07-14 10:32:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 import gestionDB as db
 from flask_restful import Api
 from flask_restful import Resource,reqparse
+import gestion_cert.sign_csr as mcsr
 
 app = Flask(__name__)
 api = Api(app)
@@ -28,15 +29,34 @@ class getConta(Resource):
         return certificat
 api.add_resource(getConta, '/contacts', '/contacts/<int:param>')
 
+class manCert(Resource):
+    
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('csr', type=str, required=True)
+        super(manCert, self).__init__()
+
+    def get(self):
+        contacts = db.lire_contacts()
+        return contacts
+
+    def post(self):
+        args = self.parser.parse_args()
+        arg1 = args['csr']
+        certificat=mcsr.signCSR(arg1)
+        
+        # Logique pour créer un nouvel utilisateur
+        #return {'user_id': param, 'name': 'John Doe'}
+        return certificat
+api.add_resource(manCert, '/csr', '/csr/<string:csr>')
 
 
 
-
-
+'''
 @app.route('/')
 def hello():
     contacts = db.lire_contacts()
     return contacts
-
+'''
 if __name__ == '__main__':
     app.run()
