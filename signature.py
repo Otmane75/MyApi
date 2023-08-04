dd398c62d3d1caa82227df361d585f3aa521836a
Otmane75 <92949728+Otmane75@users.noreply.github.com>
2023-08-04 09:40:31 +0100

    Update signature.py

--- signature.py
+++ signature.py
@@ -1,24 +1,12 @@
 import hashlib
-import PyPDF2
 
-def calculate_sha512_pdf_hash(file_path):
-    # Ouvrir le document PDF en mode binaire
-    with open(file_path, "rb") as pdf_file:
-        # Créer un objet de hachage SHA-512
-        sha512_hash = hashlib.sha512()
+def sha512_for_pdf(pdf_path):
+    hash = hashlib.sha512()
+    with open(pdf_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash.update(chunk)
+    return hash.hexdigest()
 
-        # Initialiser un objet PDF pour lire le contenu du fichier
-        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-
-        # Calculer le hachage du contenu du document PDF
-        for page_num in range(pdf_reader.numPages):
-            page = pdf_reader.getPage(page_num)
-            sha512_hash.update(page.extractText().encode('utf-8'))
-
-        # Renvoyer le hachage sous forme hexadécimale
-        return sha512_hash.hexdigest()
-
-# Exemple d'utilisation
-document_path = "chemin/vers/votre/document.pdf"
-pdf_hash = calculate_sha512_pdf_hash(document_path)
-print("Le hash SHA-512 du document PDF est :", pdf_hash)
+pdf_path = 'document.pdf'
+pdf_hash = sha512_for_pdf(pdf_path)
+print(pdf_hash)
